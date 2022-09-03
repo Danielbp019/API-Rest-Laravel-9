d2f8d9449740d7752e9845443d77723c27d8b10d
Daniel B <76835848+Danielbp019@users.noreply.github.com>
2022-09-03 01:04:22 -0500

    v2 act3
    cambio readme y protección de rutas e instalación de santum

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # apilaravel9
  API en Laravel 9 con fines de estudio.
+    1.  API REST.
+    2.  2 versiones diferentes de API.
+    3.  Privada con login usando santum autenticacion por token.
--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,12 @@ use App\Http\Controllers\Api\V2\PostController as PostV2;
 */
 
 Route::apiResource('v1/posts', PostV1::class)
-->only(['index','show', 'destroy']);
+->only(['index','show', 'destroy'])
+->middleware('auth:sanctum');
 /* ->only('show') se usa para decir que solo habilite una ruta pero se pueden habilitar mas rutas al ponerlas dentro de [] */
 /* Ruta de prueba en el postman o en el rested addon de firefox http://localhost/apilaravel9/public/api/v1/posts/1 */
 /*Se cambia Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class) por el alias!*/
 
 Route::apiResource('v2/posts', PostV2::class)
-->only(['index','show', 'destroy']);
+->only(['index','show'])
+->middleware('auth:sanctum');//se agrega esta linea para proteger la ruta y se copia en ambas rutas.
